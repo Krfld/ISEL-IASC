c5606e0f82f5b7def89da1d0eccff172156dec08
Krfld <rodrigofldias@gmail.com>
2021-10-30 16:31:14 +0100

    .

--- 2.py
+++ 2.py
@@ -3,33 +3,41 @@ from matplotlib import pyplot
 from keras.models import Sequential
 from keras.layers.core import Dense, Reshape
 from keras import activations
+from numpy.core.fromnumeric import ndim
 
 SAMPLES = 1000
 EPOCHS = 100
 
 MATRIX_SIZE = 5
 
-dummy = [[0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0]]
+dummy = np.array([[1, 1, 0, 1, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]])
 
 
-def get_matrix(size: int):
-    return np.random.randint(2, size=(size, size))
+def get_square_matrix(size: int):
+    m = np.random.randint(2, size=(size, size))
+    print('\n', m, '\n')
+    return m
 
 
-def get_results(matrix):
-    return
+def get_matrix_outputs(matrix):
+    size = np.shape(matrix)[0]
+
+    outputs = np.zeros((2, size), dtype=np.int)
+    for row in range(size):
+        index = 1
+        for n in matrix[row, :]:
+            print(n)
 
 
 def get_model():
     model = Sequential()
-    model.add(Dense(16, input_shape=(MATRIX_SIZE,), activation=activations.tanh))
-    model.add(Dense(16, activation=activations.tanh))
+    model.add(Dense(16, input_dim=MATRIX_SIZE*2, activation=activations.relu))
+    # model.add(Dense(16, activation=activations.tanh))
     model.add(Dense(MATRIX_SIZE*MATRIX_SIZE, activation=activations.sigmoid))
-    model.add(Reshape((MATRIX_SIZE, MATRIX_SIZE)))
 
     model.compile(optimizer='adam',
                   loss='mean_squared_error',
@@ -39,7 +47,8 @@ def get_model():
 
 
 def __main__():
-    model = get_model()
+    # model = get_model()
+    get_matrix_outputs(dummy)
 
     # model.fit(, , epochs=EPOCHS, verbose='auto')
 
