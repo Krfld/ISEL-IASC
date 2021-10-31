2b88001aded4ab73513570f31a4429e6d3b61bba
Krfld <rodrigofldias@gmail.com>
2021-10-31 14:49:43 +0000

    .

--- 2.py
+++ 2.py
@@ -1,15 +1,23 @@
 import math
 import numpy as np
 from matplotlib import pyplot
-from keras.models import Sequential
 from keras.layers.core import Dense
 from keras import activations
 from keras import optimizers
+from keras import models
 
-SAMPLES = 10000
-EPOCHS = 1000
+SAMPLES = 1000000
+EPOCHS = 10
 
 MATRIX_SIZE = 5
+
+LOAD_MODEL = False
+
+sample = np.array([[1, 1, 0, 1, 0],
+                   [1, 1, 1, 1, 0],
+                   [1, 0, 1, 0, 1],
+                   [0, 1, 0, 1, 1],
+                   [1, 0, 0, 1, 0]])
 
 #
 #
@@ -55,7 +63,7 @@ def get_matrix_outputs(matrix):
 
 
 def get_model():
-    model = Sequential()
+    model = models.Sequential()
     model.add(Dense(64, input_dim=MATRIX_SIZE*2, activation=activations.relu))
     model.add(Dense(64, activation=activations.relu))
     model.add(Dense(MATRIX_SIZE**2, activation=activations.sigmoid))
@@ -74,24 +82,37 @@ def get_model():
 
 
 def __main__():
+    print('Generating target data...')
     target_data = np.array(
         [np.reshape(get_square_matrix(MATRIX_SIZE), MATRIX_SIZE**2) for i in range(SAMPLES)])
     #print('Target Data\n', target_data)
 
+    print('Generating train data...')
     train_data = np.array([get_matrix_outputs(target_data[i])
                           for i in range(target_data.shape[0])], dtype=np.int)
     #print('Train Data\n', train_data)
 
-    model = get_model()
-    history = model.fit(train_data, target_data,
-                        epochs=EPOCHS, verbose='auto', validation_split=0.2)
+    if LOAD_MODEL:
+        print('Loding model...')
+        model = models.load_model('models/2/10k_samples_1k_epochs_64x64')
+    else:
+        model = get_model()
 
-    # model.predict(test_data).round()
+        print('Training model...')
+        history = model.fit(train_data, target_data,
+                            epochs=EPOCHS, verbose='auto', validation_split=0.2)
 
-    model.save('models/2/last', save_format='tf')
+        print(f'Samples: {SAMPLES} | Epochs: {EPOCHS}')
 
-    pyplot.plot(history.history['loss'])
-    pyplot.show()
+        model.save('models/2/last', save_format='tf')
+
+        pyplot.plot(history.history['loss'])
+        pyplot.show()
+
+    result = np.reshape(model.predict(np.array([get_matrix_outputs(
+        np.reshape(sample, MATRIX_SIZE**2))])).round(), (MATRIX_SIZE, MATRIX_SIZE))
+    print(result)
+    print(np.array_equal(result, sample))
 
 
 __main__()
