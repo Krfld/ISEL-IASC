2f395a76b6b86e898f7c79f9ee10c36061c1fbec
Krfld <rodrigofldias@gmail.com>
2021-11-19 14:52:07 +0000

    .

--- Objetivo 1/1_a.py
+++ Objetivo 1/1_a.py
@@ -5,9 +5,12 @@ from keras.layers.core import Dense
 from keras import activations
 from keras import optimizers
 
-EPOCHS = 300
+EPOCHS = 1000
+
 HL_NEURONS = 2
-LR = 0.1
+LR = [0.05, 0.25, 0.5, 1, 2]
+
+ACTIVATION = activations.sigmoid
 MOMENTUUM = 0
 SHUFFLE = True
 
@@ -20,24 +23,30 @@ training_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 target_data = np.array([[0], [1], [1], [0]])
 
 model = Sequential()
-model.add(Dense(HL_NEURONS, input_dim=2, activation=activations.tanh))
-model.add(Dense(1, activation=activations.tanh))
+model.add(Dense(HL_NEURONS, input_dim=2, activation=ACTIVATION))
+model.add(Dense(1, activation=ACTIVATION))
 
-model.compile(loss='mean_squared_error',
-              optimizer=optimizers.gradient_descent_v2.SGD(
-                  learning_rate=LR,
-                  momentum=MOMENTUUM),
-              metrics=None)
+for k in range(len(LR)):
+    fig, plots = plt.subplots(2, 5)
+    fig.suptitle('LR = ' + str(LR[k]))
+    for i in range(2):
+        for j in range(5):
+            model.compile(loss='mean_squared_error',
+                          optimizer=optimizers.gradient_descent_v2.SGD(
+                              learning_rate=LR[k],
+                              momentum=MOMENTUUM),
+                          metrics=None)
 
-history = model.fit(training_data,
-                    target_data,
-                    shuffle=SHUFFLE,
-                    epochs=EPOCHS,
-                    verbose='auto')
+            history = model.fit(training_data,
+                                target_data,
+                                shuffle=SHUFFLE,
+                                epochs=EPOCHS,
+                                verbose=None)
 
-print(model.predict(training_data).round())
+            # print(model.predict(training_data).round())
 
-plt.plot(history.history['loss'], 'o')
-plt.plot([LOSS_TARGET for i in range(EPOCHS)], 'o')
-plt.savefig('last.png')
+            plots[i, j].plot(history.history['loss'], 'o')
+            plots[i, j].plot([LOSS_TARGET for i in range(EPOCHS)], 'o')
+            # plt.savefig('last.png')
+
 plt.show()
