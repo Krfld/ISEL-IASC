630fb1177d5fbbe6df3bdb0a75dcc66c4909969a
Krfld <rodrigofldias@gmail.com>
2021-11-26 18:05:54 +0000

    .

--- Objetivo 2/1.py
+++ Objetivo 2/1.py
@@ -1,17 +1,19 @@
+# %%
 import math
 import numpy as np
 import random as rnd
 import matplotlib.pyplot as plt
 
 
+# %%
 class SearchAlgorithms:
     def stochasticHillClimbing(problem, stuckIterations=10):
         current = problem.initialState()
         oldNeighbor = current.copy()
-
         stuck = 0
         while True:
-            neighbor = problem.bestNeighbor(current)  # Obtain best neighbor state from current one
+            # Obtain best neighbor state from current one
+            neighbor = problem.bestNeighbor(current)
             # print(neighbor, current, '|', problem.stateValue(neighbor), problem.stateValue(current))
 
             # Check if it's stuck in an infinite loop
@@ -31,7 +33,6 @@ class SearchAlgorithms:
     def hillClimbingWithRandomRestart(problem, iterations=10):
         bestSolution = SearchAlgorithms.stochasticHillClimbing(problem)
         bestValue = problem.stateValue(bestSolution)
-
         for i in range(iterations):
             # Obtain solution from stochastic hill climbing
             solution = SearchAlgorithms.stochasticHillClimbing(problem)
@@ -49,7 +50,6 @@ class SearchAlgorithms:
     def simulatedAnnealing(problem, schedule):
         current = problem.initialState()
         t = 0
-
         while True:
             t += 1
             T = schedule(t)
@@ -64,6 +64,7 @@ class SearchAlgorithms:
                 current = neighbor.copy()
 
 
+# %%
 class NQueens:
     def __init__(self, N):
         self.N = N
@@ -71,12 +72,20 @@ class NQueens:
         # self.state = np.array([i for i in range(1, self.N+1)])
         # np.random.shuffle(self.state)
         self.state = np.array([rnd.randint(1, self.N) for i in range(self.N)])
-        self.printState(self.state, 'Start state:')
+        self.printState(self.state, 'Initial state:')
 
     def printState(self, state, msg=''):
-        print(msg, state, '|', self.stateValue(state))
+        # print(msg, state, '|', self.stateValue(state))
+
+        # Build matrix to plot
+        board = np.array([[1 if j+1 == state[i] else 0 for j in range(self.N)] for i in range(self.N)])
+        plt.figure()
+        plt.title(msg)
+        plt.imshow(board)
+        plt.axis(False)
 
     def initialState(self):
+        # Always return the same start state
         return self.state
 
     def randomNeighbor(self, state):
@@ -97,6 +106,7 @@ class NQueens:
 
         bestValue = self.stateValue(bestState)
 
+        # Randomize order to find the best neighbor
         cols = np.array([i for i in range(self.N)])
         np.random.shuffle(cols)
 
@@ -104,9 +114,12 @@ class NQueens:
             newState = state.copy()
             for row in range(1, self.N+1):
                 if row != state[col]:
+                    # Move queen
                     newState[col] = row
+
+                    # If the new state is better than the current best
                     newValue = self.stateValue(newState)
-                    if newValue >= bestValue:
+                    if newValue > bestValue:
                         bestState = newState.copy()
                         bestValue = newValue
 
@@ -123,6 +136,7 @@ class NQueens:
         return value
 
 
+# %%
 class TravellingSalesman:
     def __init__(self, N, size=100):
         self.N = N
@@ -135,27 +149,34 @@ class TravellingSalesman:
                 city = [rnd.randint(0, size), rnd.randint(0, size)]
 
             self.state = np.append(self.state, city)
+            self.state = np.reshape(self.state, (i+1, 2))
 
-        self.state = np.reshape(self.state, (N, 2))
-        self.printState(self.state, 'Start state:')
+        self.printState(self.state, 'Cities:', False)
 
-    def printState(self, state, msg=''):
-        #print(msg, state, '|', self.stateValue(state))
+    def printState(self, state, msg='', showPath=True):
+        # print(msg, state, '|', self.stateValue(state))
 
         plt.figure()
         plt.title(msg)
-        plt.plot(*zip(*state), 'o-')
-        for i in range(self.N):
-            plt.annotate(f'  {i+1}', (state[i][0], state[i][1]))
+
+        if showPath:
+            plt.plot(*zip(*state), 'o-')
+            for i in range(self.N):
+                plt.annotate(f'  {i+1}', (state[i][0], state[i][1]))
+        else:
+            plt.plot(*zip(*state), 'o')
 
     def initialState(self):
+        # Always return the same cities with different order
         np.random.shuffle(self.state)
         return self.state
 
     def distanceBetweenCities(self, city1, city2):
+        # Obtain distance between two cities
         return math.sqrt((city1[0] - city2[0])**2 + (city1[1] - city2[1])**2)
 
     def randomNeighbor(self, state):
+        # Choose two random cities
         choices = np.array([i for i in range(self.N)])
         np.random.shuffle(choices)
 
@@ -179,6 +200,7 @@ class TravellingSalesman:
 
         bestValue = self.stateValue(bestState)
 
+        # Randomize order to find the best neighbor
         indexes = np.array([i for i in range(self.N-1)])
         np.random.shuffle(indexes)
 
@@ -191,8 +213,9 @@ class TravellingSalesman:
                 newState[i] = newState[j].copy()
                 newState[j] = tempCity
 
+                # If new state is better than the current best
                 newValue = self.stateValue(newState)
-                if newValue >= bestValue:
+                if newValue > bestValue:
                     bestState = newState.copy()
                     bestValue = newValue
 
@@ -207,8 +230,9 @@ class TravellingSalesman:
         return totalDistance
 
 
+# %%
 if __name__ == '__main__':
-    # SearchAlgorithms.hillClimbingWithRandomRestart(NQueens(20))
+    SearchAlgorithms.hillClimbingWithRandomRestart(NQueens(10))
     SearchAlgorithms.hillClimbingWithRandomRestart(TravellingSalesman(20))
 
     plt.show()
