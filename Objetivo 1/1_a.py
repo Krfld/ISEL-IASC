ce8a6312bb4542242cddc9ad738797c69ded6e3f
Krfld <rodrigofldias@gmail.com>
2021-12-05 21:53:56 +0000

    .

--- Objetivo 1/1_a.py
+++ Objetivo 1/1_a.py
@@ -1,4 +1,3 @@
-# %%
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
@@ -8,48 +7,43 @@ from keras import optimizers
 
 EPOCHS = 1000
 
-HL_NEURONS = 8
-LR = [0.05, 0.25, 0.5, 1, 2]
+HL_NEURONS = 2
+LR = np.array([0.05, 0.25, 0.5, 1, 2])
 
-ACTIVATION = activations.sigmoid
-MOMENTUUM = 0
-SHUFFLE = True
+ACTIVATION = activations.tanh
+MOMENTUM = 0
+SHUFFLE = False
 
 LOSS_TARGET = 0.1
 
-# the four different states of the XOR gate
-training_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+training_data = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+target_data = np.array([[-1], [1], [1], [-1]])
 
-# the four expected results in the same order
-target_data = np.array([[0], [1], [1], [0]])
-
-model = Sequential()
-model.add(Dense(HL_NEURONS, input_dim=2, activation=ACTIVATION))
-model.add(Dense(1, activation=ACTIVATION))
+loss_target = np.array([LOSS_TARGET for i in range(EPOCHS)])
 
 for k in range(len(LR)):
     fig, plots = plt.subplots(2, 5)
-    fig.suptitle('LR = ' + str(LR[k]))
+    fig.suptitle(f'LR = {LR[k]} | MOMENTUM = {MOMENTUM}')
     for i in range(2):
         for j in range(5):
+            model = Sequential()
+            model.add(Dense(HL_NEURONS, input_dim=2, activation=ACTIVATION))
+            model.add(Dense(1, activation=ACTIVATION))
+
             model.compile(loss='mean_squared_error',
                           optimizer=optimizers.gradient_descent_v2.SGD(
                               learning_rate=LR[k],
-                              momentum=MOMENTUUM),
+                              momentum=MOMENTUM),
                           metrics=None)
 
             history = model.fit(training_data,
                                 target_data,
                                 shuffle=SHUFFLE,
                                 epochs=EPOCHS,
-                                verbose=None)
+                                verbose=None,
+                                use_multiprocessing=True)
 
-            # print(model.predict(training_data).round())
+            plots[i, j].plot(np.array(history.history['loss']), 'o')
+            plots[i, j].plot(loss_target, 'o')
 
-            plots[i, j].plot(history.history['loss'], 'o')
-            plots[i, j].plot([LOSS_TARGET for i in range(EPOCHS)], 'o')
-            # plt.savefig('last.png')
-
-    plt.show()
-
-# %%
+plt.show()
