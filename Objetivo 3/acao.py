ecf2b336630f43ebf73ec9437e784b88466bbe0e
Krfld <rodrigofldias@gmail.com>
2022-01-06 23:04:58 +0000

    .

--- Objetivo 3/acao.py
+++ Objetivo 3/acao.py
@@ -2,3 +2,9 @@ class Acao:
     def __init__(self, dx: int, dy: int):
         self.dx = dx
         self.dy = dy
+
+    def __eq__(self, other) -> bool:
+        return self.dx == other.dx and self.dy == other.dy
+
+    def __hash__(self) -> int:
+        return hash((self.dx, self.dy))
