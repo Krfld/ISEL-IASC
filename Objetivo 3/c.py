9b10173b1b7c52d1729283ac1e6506e7b68049e5
Krfld <rodrigofldias@gmail.com>
2022-01-12 17:49:48 +0000

    .

--- Objetivo 3/c.py
+++ Objetivo 3/c.py
@@ -11,13 +11,17 @@ mundo = [[0, 0, 0, 0, 0, 0, 0, -1, 1],
 
 
 class Mundo:
-    def __init__(self, mundo: list[list[int]], estadoInicial: Estado = Estado(0, 0), rMultiplicador: float = 1, custo: float = 0.01):
+    def __init__(self, mundo: list[list[int]], estadoInicial: Estado = Estado(0, 0), rMultiplicador: float = 1, custo: float = 0.01, mostrarGrafico: bool = True):
         self.mundo = mundo
         self.s = estadoInicial
         self.rMultiplicador = rMultiplicador
         self.custo = custo
-
+        self.mostrarGrafico = mostrarGrafico
         self.movimentos = 0
+
+        if self.mostrarGrafico:
+            plt.ion()
+            plt.figure()
 
     def estadoAtual(self) -> Estado:
         return self.s
@@ -35,29 +39,25 @@ class Mundo:
         r = self.mundo[sn.y][sn.x]
         return sn if not r < 0 else self.s, r*self.rMultiplicador - self.custo
 
-    def mostrar(self, mostrarGrafico: bool = True):
-        print(self.s, self.movimentos)
+    def mostrar(self):
+        # print(self.s, self.movimentos)
 
-        if not mostrarGrafico:
-            return
-
-        posicao = [[x for x in y] for y in self.mundo]
-        posicao[self.s.y][self.s.x] = 2
-        plt.title("Movimentos: " + str(self.movimentos))
-        plt.imshow(posicao)
-        plt.pause(0.1)
-        plt.show()
+        if self.mostrarGrafico:
+            posicao = [[x for x in y] for y in self.mundo]
+            posicao[self.s.y][self.s.x] = 2
+            plt.title("Movimentos: " + str(self.movimentos))
+            plt.imshow(posicao)
+            plt.pause(0.1)
+            plt.show()
 
 
 if __name__ == '__main__':
-    #                       Right     | Left       | Up         | Down
+    #                         Right     | Left       | Up         | Down
     mar = MecanismoAprendRef([Acao(1, 0), Acao(-1, 0), Acao(0, -1), Acao(0, 1)])
     estadoInicial = Estado(0, 2)
 
-    plt.ion()
     while True:
-        m = Mundo(mundo, estadoInicial)
-        plt.figure()
+        m = Mundo(mundo, estadoInicial, mostrarGrafico=False)
         while True:
             a = mar.selecionar_acao(m.estadoAtual())
             sn, r = m.mover(a)
@@ -68,3 +68,4 @@ if __name__ == '__main__':
 
             if (sn == Estado(8, 0)):
                 break
+        print(m.movimentos)
--- Objetivo 3/mecanismo_aprend_ref.py
+++ Objetivo 3/mecanismo_aprend_ref.py
@@ -5,8 +5,8 @@ class MecanismoAprendRef:
     def __init__(self, acoes: list[Acao]):
         self.acoes = acoes
         self.mem_aprend = MemoriaEsparsa()
-        self.sel_acao = EGreedy(self.mem_aprend, self.acoes, 0.2)
-        self.aprend_ref = DynaQ(self.mem_aprend, self.sel_acao, 0.7, 0.9, 500)  # confirm alpha & gama
+        self.sel_acao = EGreedy(self.mem_aprend, self.acoes, 0.1)
+        self.aprend_ref = DynaQ(self.mem_aprend, self.sel_acao, 0.5, 0.95, 1000)
 
     def aprender(self, s: Estado, a: Acao, r: float, sn: Estado):
         self.aprend_ref.aprender(s, a, r, sn)
