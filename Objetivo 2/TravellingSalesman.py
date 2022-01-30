f33f1f0019263f554d4908436e6995cb0c2e45d0
Krfld <rodrigofldias@gmail.com>
2022-01-30 17:00:33 +0000

    .

--- Objetivo 2/TravellingSalesman.py
+++ Objetivo 2/TravellingSalesman.py
@@ -20,7 +20,7 @@ class TravellingSalesman:
             print(self.state)
             self.printState(self.state, 'Cities', False)
 
-    def printState(self, state: list, msg: str = '', showPath: bool = True):
+    def printState(self, state: list[tuple], msg: str = '', showPath: bool = True):
         # print(msg, state, '|', self.stateValue(state))
 
         plt.figure()
@@ -47,11 +47,11 @@ class TravellingSalesman:
         rnd.shuffle(self.state)
         return self.state.copy()
 
-    def distanceBetweenCities(self, city1: tuple, city2: tuple):
+    def distanceBetweenCities(self, city1: tuple, city2: tuple) -> float:
         # Calculate distance between two cities
         return math.sqrt((city1[0] - city2[0])**2 + (city1[1] - city2[1])**2)
 
-    def randomNeighbor(self, state: list):
+    def randomNeighbor(self, state: list) -> list[tuple]:
         randomState = state.copy()
 
         # Choose two random cities
@@ -67,7 +67,7 @@ class TravellingSalesman:
 
         return randomState
 
-    def bestNeighbor(self, state: list):
+    def bestNeighbor(self, state: list) -> list[tuple]:
         bestState = state.copy()
 
         # Guarantee that returns a different state
@@ -92,7 +92,7 @@ class TravellingSalesman:
 
         return bestState
 
-    def stateValue(self, state: list):  # distance between cities (returning to the first)
+    def stateValue(self, state: list) -> float:  # distance between cities (returning to the first)
         # Append first city at the end
         # length = len(state)
         newState = state.copy()
@@ -119,11 +119,11 @@ class TravellingSalesman:
         # child = np.reshape(child, (len(x), 2))
         return child
 
-    def population(self, populationSize: int = 100) -> list[tuple]:
+    def population(self, populationSize: int = 100) -> list[list[tuple]]:
         population = [self.initialState() for i in range(populationSize)]
         # population = np.reshape(population, (populationSize, self.N*2))
         return population
 
-    def fitnessFunction(self, element: list) -> float:
+    def fitnessFunction(self, element: list[tuple]) -> float:
         value = self.stateValue(element)*-1
         return 1/(value+1)  # int(1000000/(value+1))
